26208191322772427057a39c143c511760cebd8d
Alexey Abramenko <a.abramenko@chita.ru>
2022-12-10 22:26:08 +0900

    Add. Users Controller

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,24 @@
 import express, {Express} from "express";
-import {userRouter} from "./users/users";
 import {Server} from "node:http";
 import {LoggerService} from "./logger/logger.service";
+import {UsersController} from "./users/users.controller";
 
 export class App {
     private app: Express;
     private port: number = 8900;
     private server: Server;
 
-    constructor(private logger: LoggerService) {
+    constructor(private logger: LoggerService, private usersController: UsersController) {
         this.app = express();
+    }
+    protected useRoutes() {
+        console.log(this.usersController.router);
+        this.app.use('/users', this.usersController.router);
     }
     public async init () {
         this.useRoutes();
         this.server = this.app.listen(this.port, () => {
             this.logger.log(`Сервер запущен на http://localhost:${this.port}/`);
         })
-    }
-    public useRoutes() {
-        this.app.use('/users', userRouter);
     }
 }
--- src/users/users.ts
+++
@@ -1,9 +0,0 @@
-import express, {NextFunction, Request, Response} from 'express';
-
-const userRouter = express.Router();
-
-userRouter.use((req: Request, res: Response, next: NextFunction) => {
-   next();
-});
-
-export {userRouter};
