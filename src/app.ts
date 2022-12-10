d6418ca6c549eab3020a2afc9352404bf348dd71
Alexey Abramenko <a.abramenko@chita.ru>
2022-12-10 21:24:41 +0900

    Add. Logger

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,20 @@
 import express, {Express} from "express";
 import {userRouter} from "./users/users";
 import {Server} from "node:http";
+import {LoggerService} from "./logger/logger.service";
 
 export class App {
     private app: Express;
     private port: number = 8900;
     private server: Server;
-    constructor() {
+
+    constructor(private logger: LoggerService) {
         this.app = express();
     }
     public async init () {
         this.useRoutes();
         this.server = this.app.listen(this.port, () => {
-            console.log(`Сервер запущен на http://localhost:${this.port}/`);
+            this.logger.log(`Сервер запущен на http://localhost:${this.port}/`);
         })
     }
     public useRoutes() {
